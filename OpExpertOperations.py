ce5358ba4946d238e4c16c308b164272c0d13973
vijayanish11 <vijayanish11920@gmail.com>
2025-04-15 16:26:26 +0300

    Changed url

--- OpExpertOperations.py
+++ OpExpertOperations.py
@@ -13,7 +13,7 @@ class Interactions:
         
         self.username = 'vanish'
         self.password = 'BitsVijay1109'        
-        self.CRMURL = 'https://app02.opexpert.com/custom/service/v4_1_custom/rest.php'
+        self.CRMURL = 'https://apigw.demo.opexpert.io/custom/service/v4_1_custom/rest.php'
         self.sessionID = False
 
     def login(self):
